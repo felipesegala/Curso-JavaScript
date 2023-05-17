76ff1a0cb4adbfa030df3fc45b95289814510467
Felipe Segala <felipe_segala@outlook.com>
2023-05-17 09:31:28 -0300

    tipos de dados number

---
+++ 5_Tipos_de_dados/script.js
@@ -0,0 +1,25 @@
+let numero = 1;
+
+console.log(numero);
+console.log(typeof numero);
+
+let float = 15.32;
+
+console.log(float);
+console.log(typeof float);
+
+let textoComNumero = '422';
+
+console.log(textoComNumero);
+console.log(typeof textoComNumero);
+
+let somaStr = 25
+
+// NÃO FAZER A SOMA DE NUMERO COM STRING;
+console.log(somaStr + textoComNumero); 
+console.log(typeof somaStr);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(typeof +Infinity);
+console.log(typeof -Infinity);
