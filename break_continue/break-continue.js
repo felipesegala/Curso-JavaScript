3939a344bcc55bde06f47405ee6dc3989b543ada
Felipe Segala <felipe_segala@outlook.com>
2023-05-19 09:46:58 -0300

    breack continue

---
+++ break_continue/break-continue.js
@@ -0,0 +1,25 @@
+for(let i = 10; i > 0; i--) {
+
+    console.log(i); // me mostra o decremento
+
+    if(i === 5) {
+        break;  // para o decremento
+    }
+}
+
+console.log('deu o Break');
+
+
+let x = 10;
+
+while(x < 100) {
+   
+    x += 10; //incremento de 10 em 10
+
+    if(x === 60 || x === 90) { // condição
+        console.log('CONTINUE');
+        continue; // pulará o sessenta e não mostrará a msg
+    }
+
+    console.log('testando continue', x);
+}
