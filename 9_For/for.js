e6ce91a97ebf5f0a9ad40edc2856fc0ed85f375e
Felipe Segala <felipe_segala@outlook.com>
2023-05-19 09:25:51 -0300

    for

---
+++ 9_For/for.js
@@ -0,0 +1,13 @@
+// for(let i = 0; i < 10; i++) {
+//     // console.log(i);
+// }
+
+let arr = [1, 2, 3, 4];
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for(let x = 5; x < 100; x *= 3) {
+    console.log(x);
+}
