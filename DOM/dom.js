3306e129d3c248a7b612ebf5f0ea3e7703d21d15
Felipe Segala <felipe_segala@outlook.com>
2023-05-19 14:05:07 -0300

    DOM manuseio

--- DOM/dom.js
+++ DOM/dom.js
@@ -11,3 +11,12 @@ body[0].appendChild(listaUl);
 let listaNoBody = document.getElementsByTagName('ul');
 
 console.log(listaNoBody[0]);
+
+for( let i = 0; i < lista.length; i++) {
+    let liFor = document.createElement('li');
+    let textoLi = document.createTextNode(lista[i]);
+    liFor.appendChild(textoLi);
+
+    listaNoBody[0].appendChild(liFor);
+}
+console.log(lista[i]);
