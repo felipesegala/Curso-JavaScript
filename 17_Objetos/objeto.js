da6a98a4aaf1d53f9ca09f358f198c5c37e29318
Felipe Segala <felipe_segala@outlook.com>
2023-05-22 10:59:50 -0300

    objetos

--- 17_Objetos/objeto.js
+++ 17_Objetos/objeto.js
@@ -2,7 +2,19 @@
 
 let pessoas = {
     nome: 'Felipe',
-    idade: 32
+    idade: 32,
+    falar: function( ){ //método dentro do objeto;
+        console.log('Olá, tudo bem?');
+    },
+    soma: function(a, b) {
+        return a + b;
+    }
 };
 
 console.log(pessoas.nome);
+console.log(pessoas.idade);
+pessoas.falar(); // acesso o método dentro do objeto.
+
+let soma = pessoas.soma(5, 5); 
+/*aqui crio a variável para que receba o retorno da função e eu possa reutilizar esse retorno ou somente mostra-lo.*/
+console.log(soma);
