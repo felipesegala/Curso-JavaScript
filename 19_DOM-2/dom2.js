993f43ec38a73995e7793e08220d3260f32ba8f7
Felipe Segala <felipe_segala@outlook.com>
2023-05-23 09:09:18 -0300

    Acessar DOM

---
+++ 19_DOM-2/dom2.js
@@ -0,0 +1,22 @@
+// tag, nesse exemplo abaixo consigo acessar o h1 somente.
+
+let titulo = document.getElementsByTagName('h1')[0];
+
+console.log(titulo);
+
+let lis = document.getElementsByTagName('li');
+
+console.log(lis);
+
+//id
+
+let paragrafo = document.getElementById('paragrafo');
+
+console.log(paragrafo);
+
+//class
+
+let itensDaLista = document.getElementsByClassName('item');
+
+console.log(itensDaLista);
+
