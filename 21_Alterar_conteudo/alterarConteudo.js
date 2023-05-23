2e139e06bc95b87d99f6470a48a725ba40a18db7
Felipe Segala <felipe_segala@outlook.com>
2023-05-23 09:35:11 -0300

    innerHtml e textContent

---
+++ 21_Alterar_conteudo/alterarConteudo.js
@@ -0,0 +1,14 @@
+let title = document.querySelector('#title');
+
+console.log(title);
+
+// innerHTML
+title.innerHTML = 'Testando o texto alterado';
+
+//textContent -> Mais utilizado
+
+let sub = document.querySelector('.subtitle');
+
+console.log(subtitle);
+
+subtitle.textContent = 'Testando textContent';
