056a8e51f8ad5a8f9f076ed8acaffbaa6fdd5b1d
Felipe Segala <felipe_segala@outlook.com>
2023-05-23 10:12:49 -0300

    remove

--- 23_remover_elementos/removerElementos.js
+++ 23_remover_elementos/removerElementos.js
@@ -1,46 +1,18 @@
-//Criar elemento no HTML, TAG;
-let novoParagrafo = document.createElement('p');
+//REMOVENDO ELEMENTO FILHO
 
-//Aqui eu crio o texto que vai ser inserido no HTML
-let texto = document.createTextNode('Este é o texto do parágrafo');
+// AQUI EU SELECIONO O PAI
+let container = document.querySelector('#container');
 
-//Aqui eu insiro o novo conteudo na tag criada
-novoParagrafo.appendChild(texto);
+//AQUI EU SELECIONO O FILHO A SER REMOVIDO
+let p = document.querySelector('#container p');
 
-console.log(novoParagrafo);
+//AQUI EU REMOVO O ELEMENTO
+container.removeChild(p);
 
-//Aqui eu mapeio aonde eu quero inserir o novo elemento, no caso é o body, que seria o pai do elemento novo;
-let body = document.querySelector('body');
+///////////////////////////////////
 
-console.log(body);
+//REMOVER O ELEMENTO EM SI
 
-//Aqui sim eu insiro finalmente o elemento novo no HTML com appendChild
-body.appendChild(novoParagrafo);
+let subtitle = document.querySelector('.subtitle');
 
-////////////////////////////////////////////
-//INSERIR NO CONTAINER
-
-//AQUI EU MAPEIO AONDE QUERO INSERIR O NOVO ELEMENTO
-let novoP = document.querySelector('#container');
-
-console.log(novoP);
-
-//AQUI EU CRIO O ELEMENTO NOVO
-let elemento = document.createElement('span');
-
-//AQUI EU INSIRO O NOVO TEXTO NO ELEMENTO
-elemento.appendChild(document.createTextNode('Texto do span'));
-
-console.log(elemento);
-
-//AQUI EU O INSIRO NO HTML 
-novoP.appendChild(elemento);
-
-/////////////////////////////////////////////////
-
-let h3 = document.querySelector('.h3');
-
-let h3Novo = document.createTextNode('Texto do H3');
-
-h3.appendChild(h3Novo);
-
+subtitle.remove();
