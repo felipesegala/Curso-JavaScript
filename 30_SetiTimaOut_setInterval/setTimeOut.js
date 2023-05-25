f9617f4ed77d429c7e2146508e7aece78c1c4b9b
Felipe Segala <felipe_segala@outlook.com>
2023-05-25 09:29:43 -0300

    clearInterval

--- 30_SetiTimaOut_setInterval/setTimeOut.js
+++ 30_SetiTimaOut_setInterval/setTimeOut.js
@@ -6,10 +6,17 @@ console.log('Antes do setTimOut');
 setTimeout(function() {
 console.log('Testando o setTimeOut');
 
-}, 2000) //aqui no final coloco o tempo de espera que quero para realizar a ação em milisegundos, sendo para cada segundo são 1000 milisegundos.
+}, 2000); //aqui no final coloco o tempo de espera que quero para realizar a ação em milisegundos, sendo para cada segundo são 1000 milisegundos.
 
 console.log('Depois do setTimOut');
 
 //Mesmo tendo instruções antes e depois do setTimOut ele só realizará sua ação após o tempo determinado.
 
 //Uma função que é passada como argumento para outra é considerada uma CallBack.
+
+// setInterval, só para quando coloco uma instrução de parada
+
+/*setInterval(function() {
+    console.log('Testando interval');
+
+}, 1000);*/
---
+++ 31_clearTimeOut_clearInterval/clearTimeOut.js
@@ -0,0 +1,25 @@
+let x = 0;
+
+// Para setar a parada eu atribuo o setTimeOut a uma variavel para poder utilizar numa if;
+
+let mytimer = setTimeout(function(){
+    console.log('O x é 0');
+}, 1500) 
+
+x = 5;
+
+if(x > 0) {
+    clearTimeout(mytimer);
+    console.log('O x passou de 0');
+}
+
+// clearInterval
+
+let myInterval = setInterval(function() {
+    console.log('Imprimindo interval');
+}, 500);
+
+setTimeout(function(){
+    console.log('Não precisamos mais repetir!');
+    clearInterval(myInterval);
+}, 1500);
