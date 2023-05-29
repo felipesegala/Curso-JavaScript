bd68658ca20affb4756b2b6082300bd4a4433997
Felipe Segala <felipe_segala@outlook.com>
2023-05-29 09:26:34 -0300

    JSON

---
+++ 36_JSON/json.js
@@ -0,0 +1,16 @@
+/* JSON = JavaScript Objetc Notation;
+Utiliza o formato de chave e valor; (q nem objeto)
+Muito utilizado em API e também arquivos de configuração;
+
+/////////////
+
+Tipos de Dados
+
+Aceita diversos tipos de dados
+Strings
+Números
+Arrays
+Objetos - {"nome": "Matheus"}; ASPAS DUPLAS
+null
+
+*/
