eeb081b043732802345fe598aaf1c6ebe073b6e9
Felipe Segala <felipe_segala@outlook.com>
2023-05-29 09:39:12 -0300

    converter json

--- 36_JSON/json.js
+++ 36_JSON/json.js
@@ -14,3 +14,43 @@ Objetos - {"nome": "Matheus"}; ASPAS DUPLAS
 null
 
 */
+
+const objs = [
+    {
+    "nome": "Felipe",
+    "idade": 32,
+    "esta_trabalhando": false,
+    "detalhes_profissao": {
+    "profissao": "Programador",
+    "empresa": "Empresa x"  
+    },
+    "hobbies": ["Programar", "Estudar", "Games"]
+    },
+    {
+    "nome": "Paty",
+    "idade": 26,
+    "esta_trabalhando": true,
+    "detalhes_profissao": {
+    "profissao": null,
+    "empresa": null  
+    },
+    "hobbies": ["Programar", "Games"]  
+    }
+]
+
+console.log(objs);
+
+
+// CONVERTER OBJETO PARA JSON
+
+const jsonData = JSON.stringify(objs);
+console.log(jsonData);
+
+//CONVERTER JSON PARA OBJETO
+
+const objData = JSON.parse(jsonData);
+console.log(objData);
+
+objData.map((pessoa) => {
+    console.log(pessoa.nome);
+});
